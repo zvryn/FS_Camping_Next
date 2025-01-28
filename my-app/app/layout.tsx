eefaf438660e6cf61b8709d838d718ea2177416b
zvryn <sarinkanok@gmail.com>
2025-01-28 14:08:02 +0700

    install shadCN

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+const layout = ({ children }) => {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default layout;
---
+++ my-app/app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div className="text-black">test2</div>;
+};
+
+export default HomePage;
